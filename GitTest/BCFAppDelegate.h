e834166c17871140f6652e4e18bf1f0098a1e233
Bridget <excel@student-11-27.foxcroftacademy.lan>
2014-06-25 14:35:10 -0400

    Initial Commit

---
+++ GitTest/BCFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BCFAppDelegate.h
+//  GitTest
+//
+//  Created by Bridget on 6/25/14.
+//  Copyright (c) 2014 Bridget Ferry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BCFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitTest/BCFViewController.h
@@ -0,0 +1,13 @@
+//
+//  BCFViewController.h
+//  GitTest
+//
+//  Created by Bridget on 6/25/14.
+//  Copyright (c) 2014 Bridget Ferry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BCFViewController : UIViewController
+
+@end
